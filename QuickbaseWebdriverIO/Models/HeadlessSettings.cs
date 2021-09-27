48cd0cb12fe3478787986eee89e171dd5e086bd5
Yordan Georgiev <Yordan.Georgiev@ascentsoftware.eu>
2021-09-27 17:17:41 +0300

    Removed cached

--- QuickbaseWebdriverIO/Models/HeadlessSettings.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace QuickbaseWebdriverIO.Models
-{
-    public class HeadlessSettings
-    {
-        public bool IsEnabled { get; set; }
-
-        public string ScreenshotResolution { get; set; }
-    }
-}
